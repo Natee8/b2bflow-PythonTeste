0a4e04ba62effc7087967cdec5f80134f4a592fb
Nathália Almeida Amorim <136372432+Natee8@users.noreply.github.com>
2025-08-14 03:18:10 -0300

    feat: formartar o numero para padrão

---
+++ utils/formatter.py
@@ -0,0 +1,16 @@
+import re
+
+#opcional para enviar somente para numeros brasileiros
+
+def format_phone(number: str):
+    if not number:
+        return ""
+    clean_number = re.sub(r"\D", "", number)
+
+    if clean_number.startswith("55"):
+        return clean_number
+    
+    if clean_number.startswith("0"):
+        return clean_number
+    
+    return f"55{clean_number}"
