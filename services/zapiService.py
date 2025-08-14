5034640de99bc44aba1befce054e35aa0d4b3b2d
Nathália Almeida Amorim <136372432+Natee8@users.noreply.github.com>
2025-08-14 04:04:23 -0300

    fix: corrigir autenticação do token da Z-API e ajustando variáveis de ambiente

--- services/zapiService.py
+++ services/zapiService.py
@@ -5,38 +5,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ZAPI_INSTANCE_ID = os.getenv("ZAPI_INSTANCE_ID")
-ZAPI_TOKEN = os.getenv("ZAPI_TOKEN")
+ZAPI_INSTANCE_TOKEN = os.getenv("ZAPI_INSTANCE_TOKEN")
+ZAPI_CLIENT_TOKEN = os.getenv("ZAPI_CLIENT_TOKEN")
 
-ZAPI_BASE_URL = f"https://api.z-api.io/instances/{ZAPI_INSTANCE_ID}/send-text"
+ZAPI_BASE_URL = f"https://api.z-api.io/instances/{ZAPI_INSTANCE_ID}/token/{ZAPI_INSTANCE_TOKEN}"
 
 def sendMessage(name: str, number: str):
+    url = f"{ZAPI_BASE_URL}/send-text"
     text = f"Olá {name}, tudo bem com você?!"
+
+    headers = {
+        "Client-Token": ZAPI_CLIENT_TOKEN,
+        "Content-Type": "application/json"
+    }
+
     payload = {
         "phone": str(number),
         "message": text
     }
-    headers = {
-        "Client-Token": ZAPI_TOKEN
-    }
 
     try:
-        response = requests.post(ZAPI_BASE_URL, json=payload, headers=headers)
-        
+        response = requests.post(url, json=payload, headers=headers)
         if response.status_code == 200:
-            return {
-                "status": "success",
-                "message": f"Mensagem enviada para {name} ({number})"
-            }
+            return {"status": "success", "message": f"Mensagem enviada para {name} ({number})"}
         else:
-            return {
-                "status": "error",
-                "message": f"Erro ao enviar para {name} ({number})",
-                "details": response.text
-            }
-
+            return {"status": "error", "message": f"Erro ao enviar para {name} ({number})", "details": response.text}
     except requests.RequestException as e:
-        return {
-            "status": "error",
-            "message": "Erro ao conectar com Z-API",
-            "details": str(e)
-        }
+        return {"status": "error", "message": "Erro ao conectar com Z-API", "details": str(e)}
